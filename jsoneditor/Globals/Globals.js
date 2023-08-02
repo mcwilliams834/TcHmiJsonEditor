6ea6556e31cb641e81fa4f39f10289657f1af533
mcwilliams834 <mcwilliams834@gmail.com>
2023-08-02 10:06:46 -0500

    add globals js file to make the editor in global scope

---
+++ jsoneditor/Globals/Globals.js
@@ -0,0 +1,3 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.760.44/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+var editor;
